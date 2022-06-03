f1bf6f1179e36a6b4bf7651e6d56fa0779ad58b8
shafi- <shafi.cse.buet@gmail.com>
2022-06-04 05:18:36 +0600

    Configure mongo db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import DbModule from 'src/modules/db/db.module';
+import { AppController } from 'src/app.controller';
+import { AppService } from 'src/app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DbModule,
     Logger,
   ],
   controllers: [AppController],
