abf9207179f517c8970405b239263107a3052691
shafi- <shafi.cse.buet@gmail.com>
2022-06-04 01:46:20 +0600

    Configure dotenv based config module

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import Env from './Env';
+
+const Constants = {
+  Env,
+};
+
+export default Constants;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import Constants from 'src/constants';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = await app.resolve<ConfigService>(ConfigService);
+
+  await app.listen(configService.get(Constants.Env.APP_PORT));
+
+  const logger = await app.resolve<Logger>(Logger);
+
+  logger.log(
+    `Nest Application is running at http://localhost:${configService.get(
+      Constants.Env.APP_PORT,
+    )}`,
+  );
 }
+
 bootstrap();
