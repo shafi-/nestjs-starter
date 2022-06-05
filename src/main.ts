70f79c8a33bb2a1da70e2dfa92e4f394d216067c
shafi- <shafi.cse.buet@gmail.com>
2022-06-05 18:48:05 +0600

    Swagger configured

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,39 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import Constants from 'src/constants';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const logger = new Logger('NestApplication');
   const configService = await app.resolve<ConfigService>(ConfigService);
 
-  await app.listen(configService.get(Constants.Env.APP_PORT));
+  const APP_PORT = configService.get(Constants.Env.APP_PORT);
+  const isSwaggerEnabled = configService.get<boolean>('SWAGGER_ENABLE', false);
 
-  const logger = new Logger('NestApplication');
+  if (isSwaggerEnabled) {
+    const config = new DocumentBuilder()
+      .setTitle(configService.get('SWAGGER_TITLE', 'API docs'))
+      .setDescription(configService.get('SWAGGER_DESC', 'The API description'))
+      .setVersion(configService.get('SWAGGER_VERSION', '1.0'))
+      .addTag(configService.get('SWAGGER_TAG', 'nestjs-starter'))
+      .build();
 
-  logger.log(
-    `Nest Application is running at http://localhost:${configService.get(
-      Constants.Env.APP_PORT,
-    )}`,
-  );
+    const document = SwaggerModule.createDocument(app, config);
+    const swaggerPrefix = configService.getOrThrow('SWAGGER_PREFIX');
+    SwaggerModule.setup(swaggerPrefix, app, document);
+
+    logger.log(
+      `Swagger doc is running at http://localhost:${APP_PORT}${swaggerPrefix}`,
+    );
+  }
+
+  await app.listen(APP_PORT);
+
+  logger.log(`Nest Application is running at http://localhost:${APP_PORT}`);
 }
 
 bootstrap();
